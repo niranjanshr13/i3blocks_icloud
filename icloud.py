71a251dd97fb72fc64ffcf51995b2f19221fe682
niranjanshr13 <niranjanshr13@gmail.com>
2017-10-28 01:50:19 -0400

    need to add battery and fixing location

--- icloud.py
+++ icloud.py
@@ -12,24 +12,34 @@ def deviceCount(device):
     devices = len(dict(device))
     return devices
 
+
+def batteryLevel(device):
+    pass
+
+
+
 def findLocation(device, devices):
     for x in range(devices):
         try:
             deviceName = str(device[x]).split(':')[1].replace(' ','')
             deviceLocation = device[x].location()
-            print(deviceLocation)
+            if deviceLocation == 'None':
+                pass
+            #print(deviceLocation)
             deviceLatitude = deviceLocation['latitude']
             deviceLongitude = deviceLocation['longitude']
+            print(deviceLatitude)
+            print(deviceLongitude)
             unixTime = int(time.time())
-            url = 'https://maps.googleapis.com/maps/api/js/GeocodeService.Search?5m2&1d' + str(deviceLatitude) + '&2d' + str(deviceLongitude)  + '&7sUS&9sen-US&key=AIzaSyCIDv7kXdaN-zt4bLG-h9TdWgk42HPgQ80&callback=_xdc_._rkedz2&token=86596'
-            print(url)
+#            url = 'https://maps.googleapis.com/maps/api/js/GeocodeService.Search?5m2&1d' + str(deviceLatitude) + '&2d' + str(deviceLongitude)  + '&7sUS&9sen-US&key=AIzaSyCIDv7kXdaN-zt4bLG-h9TdWgk42HPgQ80&callback=_xdc_._rkedz2&token=86596'
+#            print(url)
             #url = 'https://www.findlatitudeandlongitude.com/processors/get-reverse-geocode.php?lat_in=' + str(deviceLatitude) + '&lon_in=' + str(deviceLongitude)
             #url = 'https://www.findlatitudeandlongitude.com/processors/get-reverse-geocode.php?lat_in=' + str(deviceLatitude) + '&lon_in=' + str(deviceLongitude) + '&time=' + str(unixTime)
-            r = requests.get(url).text
-            print(r)
-            j = json.loads(r)
-            d = j['results'][0]['formatted_address']
-            print(d)
+#            r = requests.get(url).text
+#            print(r)
+#            j = json.loads(r)
+#            d = j['results'][0]['formatted_address']
+#            print(d)
         except:
             pass
 
